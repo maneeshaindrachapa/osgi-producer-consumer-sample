585eca33b646ab38dd9f651a0e246be7acc9421e
Jürgen Albert <j.albert@data-in-motion.biz>
2021-08-02 12:18:00 +0200

    Simplified the Scenario
    Signed-off-by: Jürgen Albert <j.albert@data-in-motion.biz>

--- org.wso2.carbon.game.consumer/src/main/java/org/wso2/carbon/game/consumer/GameConsumer.java
+++ org.wso2.carbon.game.consumer/src/main/java/org/wso2/carbon/game/consumer/GameConsumer.java
@@ -1,11 +1,27 @@
 package org.wso2.carbon.game.consumer;
 
-import org.wso2.carbon.game.consumer.internal.GameConsumerDataHolder;
+import java.util.logging.Logger;
 
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.wso2.carbon.game.producer.GameProducer;
+
+@Component(
+		service = {}, // This is the default if no interface is implemented and therefore actually unnecessary. Thus this is no Service but a Simple component.
+		immediate = true // This is the default for a Component (not a Service), therefore actually unnecessary and it will be activated as soon as all mandatory references are available
+		)
 public class GameConsumer {
 
-    public void getChampionCreated(String championName) {
+	private static final Logger LOGGER = Logger.getLogger(GameConsumer.class.getName());
 
-        GameConsumerDataHolder.getInstance().getGameProducer().createChampion(championName);
+	@Reference // Will be mandatory by default, as this is a field
+	GameProducer producer;
+	
+    @Activate
+    protected void activate(ComponentContext context) {
+        LOGGER.info("Game consumer Component is activated");
+        producer.createChampion("Zed");
     }
 }
